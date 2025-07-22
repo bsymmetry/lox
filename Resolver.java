b6237f218ca9278212f4f27a034b111ef88d0800
bsymmetry <merrillt@seas.upenn.edu>
2025-07-22 18:22:12 -0400

    adding class

--- Resolver.java
+++ Resolver.java
@@ -8,9 +8,15 @@ import java.util.Stack;
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	private final Interpreter interpreter;
 	private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+	private FunctionType currentFunction = FunctionType.NONE;
 
 	Resolver(Interpreter interpreter) {
 		this.interpreter = interpreter;
+	}
+
+	private enum FunctionType {
+		NONE,
+		FUNCTION
 	}
 
 	@Override
@@ -18,6 +24,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		beginScope();
 		resolve(stmt.statements);
 		endScope();
+		return null;
+	}
+
+	@Override
+	public Void visitClassStmt(Stmt.Class stmt) {
+		declare(stmt.name);
+		define(stmt.name);
 		return null;
 	}
 
@@ -32,7 +45,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		declare(stmt.name);
 		define(stmt.name);
 		
-		resolveFunction(stmt);
+		resolveFunction(stmt, FunctionType.FUNCTION);
 		return null;
 	}
 
@@ -52,6 +65,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitReturnStmt(Stmt.Return stmt) {
+		if (currentFunction == FunctionType.NONE) [
+			Lox.error(stmt.keyword, "Can't return from top-level code.");
+		}
+
 		if (stmt.value != null) {
 			resolve(stmt.value);
 		}
@@ -161,6 +178,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		if (scopes.isEmpty()) return;
 
 		Map<String, Boolean> scope = scopes.peek();
+		if (scope.containsKey(name.lexeme)) {
+			Lox.error(name,
+			    "Already a variable with this name in this scope.");
+		}
+
 		scope.put(name.lexeme, false);
 	}
 
@@ -178,7 +200,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		}
 	}
 
-	private void resolveFunction(Stmt.Function function) {
+	private void resolveFunction(
+			Stmt.Function function, FunctionType type) {
+		FunctionType enclosingFunction = currentFunction;
+		currentFunction = type;
+
 		beginScope();
 		for (Token param: function.params) {
 			declare(param);
@@ -186,6 +212,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		}
 		resolve(function.body);
 		endScope();
+		currentFunction = enclosingFunction;
 	}
 
 }
