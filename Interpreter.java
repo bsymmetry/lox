5c1e11cf4d4d4ef1646fe525a6b60b239e17c61b
bsymmetry <merrillt@seas.upenn.edu>
2025-07-24 11:07:36 -0400

    added superclasses and inheritance

--- Interpreter.java
+++ Interpreter.java
@@ -71,6 +71,23 @@ class Interpreter implements Expr.Visitor<Object>,
 	}
 
 	@Override
+	public Object visitSuperExpr(Expr.super expr) {
+		int distance = locals.get(expr);
+		LoxClass superclass = (LoxClass)environment.getAt(
+			distance, "super");
+
+		LoxInstance object = (LoxInstance)environment.getAt(
+			distance - 1, "this");
+
+		LoxFunction method = superclass.findMethod(expr.method.lexeme);
+
+		if (method == null) {
+			throw new RuntimeError(expr.method,
+			"Undefined property '" + expr.method.lexeme + "'.");
+		return method.bind(object);
+	}
+
+	@Override
 	public Object visitThisExpr(Expr.This expr) {
 		return lookUpVariable(expr.keyword, expr);
 	}
@@ -181,7 +198,29 @@ class Interpreter implements Expr.Visitor<Object>,
 
 	@Override
 	public Void visitClassStmt(Stmt.Class stmt) {
+		Object superclass = null;
+		if (stmt.supperclass != null) {
+			superclass = evaluate(stmt.superclass);
+			if (!(superclass instanceof LoxClass)) {
+				throw new RuntimeError(stmt.superclass.name,
+					"Superclass must be a class.");
+			}
+		}
+
+		LoxClass klass = new LoxClass(stmt.name.lexeme,
+			(LoxClass)superclass, methods);
+
+		if (superclass != null) {
+			environment = environment.enclosing;
+		}
+
 		environment.define(stmt.name.lexeme, null);
+
+		if (stmt.superclass != null) {
+			environment = new Environment(environment);
+			environment.define("super", superclass);
+		}
+
 		Map<String, LoxFunction> methods = new HashMap<>();
 		for (Stmt.Function method : stmt.methods) {
 			LoxFunction function = new LoxFunction(method, 
